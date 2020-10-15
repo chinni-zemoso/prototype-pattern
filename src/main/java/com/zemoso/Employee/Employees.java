019f929938275bce746a5b8288e4ce95caf876f8
chinni-zemoso <chinni.srungavarapu@zemosolabs.com>
2020-10-15 16:31:00 +0530

    initial commit

--- src/main/java/com/zemoso/Employee/Employees.java
+++ src/main/java/com/zemoso/Employee/Employees.java
@@ -3,36 +3,34 @@ package com.zemoso.Employee;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Employees implements Cloneable{
+public class Employees implements Cloneable {
 
-	private List<String> empList;
-	
-	public Employees(){
-		empList = new ArrayList<String>();
-	}
-	
-	public Employees(List<String> list){
-		this.empList=list;
-	}
-	public void loadDBData(){
-		//read all employees from database and put into the list
-		empList.add("Pankaj");
-		empList.add("Raj");
-		empList.add("David");
-		empList.add("Lisa");
-	}
-	
-	public List<String> getEmpList() {
-		return empList;
-	}
+    private final List<String> empList;
 
-	@Override
-	public Object clone() throws CloneNotSupportedException{
-			List<String> temp = new ArrayList<String>();
-			for(String s : this.getEmpList()){
-				temp.add(s);
-			}
-			return new Employees(temp);
-	}
-	
+    public Employees() {
+        empList = new ArrayList<String>();
+    }
+
+    public Employees(List<String> list) {
+        this.empList = list;
+    }
+
+    public void loadDBData() {
+        //read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        super.clone();
+        return new Employees(new ArrayList<String>(this.getEmpList()));
+    }
+
 }
